ed5e817254a127a09e23661c44c4a7834486dccb
unknown <yoosehy@blinn.csenetid.cs.washington.edu>
2019-01-16 22:19:20 -0800

    Finally done

---
+++ roll_a_ball/SettingUpOculusRift/Assets/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+
+    public Text text;
+    private int count;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        text.text = "Count: " + count; 
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            count++;
+            text.text = "Count: " + count;
+            other.gameObject.SetActive(false);
+        }
+    }
+}
